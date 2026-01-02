6527ccb11fe7c8ef1031462f3da9c4d395570fbf
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2026-01-02 19:43:57 +0530

    Add files via upload

---
+++ First_Ten_Even.c
@@ -0,0 +1,21 @@
+// Q.Write a program to print first 10 Even natural numbers.
+
+
+#include<stdio.h>
+
+ 
+int main()
+{
+    int iCnt=0;
+
+    for(iCnt=1; iCnt<=10*2; iCnt++)
+    {
+      if(iCnt % 2 == 0)
+      {
+        printf("%d\t",iCnt);
+      }
+    }
+
+	return 0;
+
+}
